7a8482f3a55ddfc2fda69e4509cd59d8246dbf19
AB <www.wpj@163.com>
2012-12-04 20:07:48 +0800

    新建项目
    Signed-off-by: AB <www.wpj@163.com>

---
+++ MapGridSample/GridIndexWindow.xaml.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MapGridSample
+{
+    /// <summary>
+    /// GridIndexWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class GridIndexWindow : Window
+    {
+        const double WIDTH = 720d;
+        const double HEIGHT = 360d;
+        const double BOUNDING_BOX_X_MIN = 113.75;   // 东经
+        const double BOUNDING_BOX_Y_MIN = 22.42;    // 北纬
+        const double BOUNDING_BOX_X_MAX = 114.65;   // 东经
+        const double BOUNDING_BOX_Y_MAX = 22.87;    // 北纬
+        const int LEVEL1_GRID = 4;
+        const int LEVEL2_GRID = 4;
+        const int LEVEL3_GRID = 4;
+        const int LEVEL4_GRID = 4;
+
+        private readonly Brush _level1Brush = new SolidColorBrush(Color.FromArgb(0xff, 0xDC, 0x79, 0x1F));
+        private readonly Brush _level2Brush = new SolidColorBrush(Color.FromArgb(0xff, 0x72, 0x87, 0xC8));
+        private readonly Brush _level3Brush = new SolidColorBrush(Color.FromArgb(0xff, 0xF8, 0x31, 0x0E));
+        private readonly Brush _level4Brush = new SolidColorBrush(Color.FromArgb(0xff, 0xB1, 0xDF, 0xA9));
+
+        private int _level = 1;
+        private int _operation = 0;
+        private Point _originPoint;
+        private Positioning _offsetPositioning = new Positioning(); // 原始屏幕起始经纬度坐标
+
+        public GridIndexWindow()
+        {
+            InitializeComponent();
+            CanvasGrid.Width = WIDTH;
+            CanvasGrid.Height = HEIGHT;
+            GridCanvas.Width = WIDTH;
+            GridCanvas.Height = HEIGHT;
+            ShapCanvas.Width = WIDTH;
+            ShapCanvas.Height = HEIGHT;
+
+            DrawGrid();
+        }
+
+        #region 绘制网格
+
+        private void Window_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            var point = e.GetPosition(GridCanvas);
+            if (e.Delta > 0 && _level < 4)
+            {
+                _level++;
+                DrawGrid();
+                GridLevelTextBlock.Text = _level.ToString();
+            }
+            else if (e.Delta < 0 && _level > 1)
+            {
+                _level--;
+                DrawGrid();
+                GridLevelTextBlock.Text = _level.ToString();
+            }
+        }
+
+        private void DrawGrid()
+        {
+            switch (_level)
+            {
+                case 1:
+                    DrawLevel1Grid();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+            };
+        }
+
+        private void DrawLevel1Grid()
+        {
+            int sqrt1 = (int)Math.Sqrt(LEVEL1_GRID);
+            double lv1CellWidth = CanvasGrid.Width / sqrt1;
+            double lv1CellHeight = CanvasGrid.Height / sqrt1;
+
+            this.GridCanvas.Children.Clear();
+
+            for (int i = 0; i <= sqrt1; i++)
+            {
+                var y = i * lv1CellHeight;
+                this.GridCanvas.Children.Add(new Line()
+                {
+                    X1 = 0,
+                    X2 = 720,
+                    Y1 = y,
+                    Y2 = y,
+                    Stroke = _level1Brush,
+                    StrokeThickness = 1,
+                    Opacity = 0.5
+                });
+            }
+
+            for (int i = 0; i <= sqrt1; i++)
+            {
+                var x = i * lv1CellWidth;
+                this.GridCanvas.Children.Add(new Line()
+                {
+                    X1 = x,
+                    X2 = x,
+                    Y1 = 0,
+                    Y2 = 360,
+                    Stroke = _level1Brush,
+                    StrokeThickness = 1,
+                    Opacity = 0.5
+                });
+            }
+        }
+
+        #endregion
+
+        #region 画图
+
+        private void MoveRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            _operation = 0;
+            CanvasGrid.Cursor = Cursors.Hand;
+        }
+
+        private void LineRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            _operation = 1;
+            CanvasGrid.Cursor = Cursors.Pen;
+        }
+
+        private void PolygonRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            _operation = 2;
+            CanvasGrid.Cursor = Cursors.Cross;
+        }
+
+        private Positioning TranformVector(Vector vector)
+        {
+            Positioning positioning = new Positioning();
+            switch (_level)
+            {
+                case 1:
+                    positioning.Longitude = (BOUNDING_BOX_X_MAX - BOUNDING_BOX_X_MIN) * vector.X / WIDTH;
+                    positioning.Latitude = (BOUNDING_BOX_Y_MIN - BOUNDING_BOX_Y_MAX) * vector.Y / HEIGHT;
+                    break;
+                case 2:
+                    positioning.Longitude = (BOUNDING_BOX_X_MAX - BOUNDING_BOX_X_MIN) * vector.X / (WIDTH * LEVEL1_GRID);
+                    positioning.Latitude = (BOUNDING_BOX_Y_MIN - BOUNDING_BOX_Y_MAX) * vector.Y / (HEIGHT * LEVEL1_GRID);
+                    break;
+                case 3:
+                    positioning.Longitude = (BOUNDING_BOX_X_MAX - BOUNDING_BOX_X_MIN) * vector.X / (WIDTH * LEVEL1_GRID * LEVEL2_GRID);
+                    positioning.Latitude = (BOUNDING_BOX_Y_MIN - BOUNDING_BOX_Y_MAX) * vector.Y / (HEIGHT * LEVEL1_GRID * LEVEL2_GRID);
+                    break;
+                case 4:
+                    positioning.Longitude = (BOUNDING_BOX_X_MAX - BOUNDING_BOX_X_MIN) * vector.X / (WIDTH * LEVEL1_GRID * LEVEL2_GRID * LEVEL3_GRID);
+                    positioning.Latitude = (BOUNDING_BOX_Y_MIN - BOUNDING_BOX_Y_MAX) * vector.Y / (HEIGHT * LEVEL1_GRID * LEVEL2_GRID * LEVEL3_GRID);
+                    break;
+            };
+            return positioning;
+        }
+
+        private void CanvasGrid_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            CanvasGrid.CaptureMouse();
+
+            _originPoint = e.GetPosition(CanvasGrid);
+
+            CanvasGrid.MouseMove -= CanvasGrid_PreviewMouseMove;
+            CanvasGrid.MouseMove += CanvasGrid_PreviewMouseMove;
+            CanvasGrid.PreviewMouseLeftButtonUp -= CanvasGrid_PreviewMouseLeftButtonUp;
+            CanvasGrid.PreviewMouseLeftButtonUp += CanvasGrid_PreviewMouseLeftButtonUp;
+        }
+
+        private void CanvasGrid_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            Point point = e.GetPosition(CanvasGrid);
+            double offsetX = point.X - _originPoint.X;
+            double offsetY = point.Y - _originPoint.Y;
+            Canvas.SetLeft(GridCanvas, offsetX);
+            Canvas.SetTop(GridCanvas, offsetY);
+        }
+
+        private void CanvasGrid_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            CanvasGrid.ReleaseMouseCapture();
+            CanvasGrid.MouseMove -= CanvasGrid_PreviewMouseMove;
+            CanvasGrid.PreviewMouseLeftButtonUp -= CanvasGrid_PreviewMouseLeftButtonUp;
+
+            var vector = e.GetPosition(CanvasGrid) - _originPoint;
+
+            _offsetPositioning += TranformVector(vector);
+
+            GridCanvas.ClearValue(Canvas.LeftProperty);
+            GridCanvas.ClearValue(Canvas.TopProperty);
+        }
+
+        #endregion
+
+        #region Struct
+
+        /// <summary>
+        /// 地理经度和纬坐标
+        /// </summary>
+        struct Positioning
+        {
+            /// <summary>
+            /// 经度(东经)
+            /// </summary>
+            public double Longitude { get; set; }
+
+            /// <summary>
+            /// 纬度（北纬）
+            /// </summary>
+            public double Latitude { get; set; }
+
+            public static Positioning operator -(Positioning positioning1, Positioning positioning2)
+            {
+                Positioning result = new Positioning();
+                result.Longitude = positioning1.Longitude - positioning2.Longitude;
+                result.Latitude = positioning1.Latitude - positioning2.Latitude;
+                return result;
+            }
+
+            public static Positioning operator +(Positioning positioning1, Positioning positioning2)
+            {
+                Positioning result = new Positioning();
+                result.Longitude = positioning1.Longitude + positioning2.Longitude;
+                result.Latitude = positioning1.Latitude + positioning2.Latitude;
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// 单元格
+        /// </summary>
+        struct Cell
+        {
+            /// <summary>
+            /// 行
+            /// </summary>
+            public int Row { get; set; }
+
+            /// <summary>
+            /// 列
+            /// </summary>
+            public int Column { get; set; }
+        }
+
+        #endregion
+    }
+}
---
+++ MapGridSample/MainWindow.xaml.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MapGridSample
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const double GRID_SIZE = 20;
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            for (int i = 0; i <= 18; i++)
+            {
+                var y = i * GRID_SIZE;
+                this.GridCanvas.Children.Add(new Line() {
+                    X1 = 0,
+                    X2 = 720,
+                    Y1 = y,
+                    Y2 = y,
+                    Stroke = Brushes.Silver,
+                    StrokeThickness = 1,
+                    Opacity = 0.5
+                });
+            }
+
+            for (int i = 0; i <= 36; i++)
+            {
+                var x = i * GRID_SIZE;
+                this.GridCanvas.Children.Add(new Line()
+                {
+                    X1 = x,
+                    X2 = x,
+                    Y1 = 0,
+                    Y2 = 360,
+                    Stroke = Brushes.Silver,
+                    StrokeThickness = 1,
+                    Opacity = 0.5
+                });
+            }
+        }
+
+        private bool _isDrawing;
+        private void CellCanvas_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!_isDrawing)
+            {
+                CellCanvas.CaptureMouse();
+                CellCanvas.MouseMove += CellCanvas_PreviewMouseMove;
+                CellCanvas.PreviewMouseRightButtonDown += CellCanvas_PreviewMouseRightButtonDown;
+                TestPolyline.Points.Clear();
+                TestPolygon.Points.Clear();
+                CellCanvas.Children.Clear();
+                _isDrawing = true;
+                if (LineRadioButton.IsChecked == true)
+                {
+                    TestPolyline.Points.Add(e.GetPosition(CellCanvas));
+                }
+                else
+                {
+                    TestPolygon.Points.Add(e.GetPosition(CellCanvas));
+                }
+            }
+            if (LineRadioButton.IsChecked == true)
+            {
+                TestPolyline.Points.Add(e.GetPosition(CellCanvas));
+            }
+            else
+            {
+                TestPolygon.Points.Add(e.GetPosition(CellCanvas));
+            }
+        }
+
+        private void CellCanvas_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+
+            if (LineRadioButton.IsChecked == true)
+            {
+
+                TestPolyline.Points.RemoveAt(TestPolyline.Points.Count - 1);
+
+                TestPolyline.Points.Add(e.GetPosition(CellCanvas));
+            }
+            else
+            {
+
+                TestPolygon.Points.RemoveAt(TestPolygon.Points.Count - 1);
+
+                TestPolygon.Points.Add(e.GetPosition(CellCanvas));
+            }
+        }
+
+        private void CellCanvas_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _isDrawing = false;
+            CellCanvas.MouseMove -= CellCanvas_PreviewMouseMove;
+            CellCanvas.PreviewMouseRightButtonDown -= CellCanvas_PreviewMouseRightButtonDown;
+            CellCanvas.ReleaseMouseCapture();
+
+            var cells = LineRadioButton.IsChecked == true ? GetPolyLineRegion(TestPolyline.Points) : GetPolygonRegion(TestPolygon.Points);
+
+            foreach (var cell in cells)
+            {
+                var grid = new Rectangle();
+                grid.Fill = Brushes.Green;
+                Canvas.SetLeft(grid, cell.X * GRID_SIZE);
+                Canvas.SetTop(grid, cell.Y * GRID_SIZE);
+                grid.Width = grid.Height = GRID_SIZE;
+                CellCanvas.Children.Add(grid);
+            }
+        }
+
+        private IEnumerable<Point> GetPolygonRegion(PointCollection points)
+        {
+            var maxX = Math.Floor(points.Max(p => p.X) / GRID_SIZE);
+            var maxY = Math.Floor(points.Max(p => p.Y) / GRID_SIZE);
+            var minX = Math.Floor(points.Min(p => p.X) / GRID_SIZE);
+            var minY = Math.Floor(points.Min(p => p.Y) / GRID_SIZE);
+
+            for (double x = minX; x <= maxX; x++)
+            {
+                for (double y = minY; y <= maxY; y++)
+                {
+                    yield return new Point(x, y);
+                }
+            }
+        }
+
+        private IEnumerable<Point> GetPolyLineRegion(PointCollection points)
+        {
+            var result = new List<Point>();
+            var prev = points.First();
+            for(int i = 1; i< points.Count; i++)
+            {
+                result.AddRange(GetLineRegion(prev, points[i]));
+                prev = points[i];
+            }
+            return result.Distinct();
+        }
+
+        private IEnumerable<Point> GetLineRegion(Point startPoint, Point endPoint)
+        {
+            //  一次函数公式: kx + b = y
+            var k = (startPoint.Y - endPoint.Y) / (startPoint.X - endPoint.X);
+            var b = startPoint.Y - k * startPoint.X;
+
+            if (!double.IsInfinity(k))
+            {
+                var maxX = Math.Floor(Math.Max(startPoint.X, endPoint.X) / GRID_SIZE);
+                var minX = Math.Ceiling(Math.Min(startPoint.X, endPoint.X) / GRID_SIZE);
+                for (double x = minX; x <= maxX; x++)
+                {
+                    var y = Math.Floor((k * x * GRID_SIZE + b) / GRID_SIZE);
+                    yield return new Point(x, y);
+                    yield return new Point(x - 1, y);
+                }
+            }
+
+            if (k != 0)
+            {
+                var maxY = Math.Floor((Math.Max(startPoint.Y, endPoint.Y) / GRID_SIZE));
+                var minY = Math.Ceiling(Math.Min(startPoint.Y, endPoint.Y) / GRID_SIZE);
+                var isInfinity = double.IsInfinity(k);
+                var fixedX = Math.Floor(endPoint.X / GRID_SIZE);
+
+                for (double y = minY; y <= maxY; y++)
+                {
+                    var x = isInfinity ? fixedX : Math.Floor((y * GRID_SIZE - b) / (k * GRID_SIZE));
+                    yield return new Point(x, y);
+                    yield return new Point(x, y - 1);
+                }
+            }
+
+            yield return new Point(Math.Floor(startPoint.X / GRID_SIZE), Math.Floor(startPoint.Y / GRID_SIZE));
+            yield return new Point(Math.Floor(endPoint.X / GRID_SIZE), Math.Floor(endPoint.Y / GRID_SIZE));
+        }
+    }
+}
